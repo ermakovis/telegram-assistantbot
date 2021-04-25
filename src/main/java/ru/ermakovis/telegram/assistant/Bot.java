57af052c0e373145c2a17d0a63b68188686577c3
Илья Ермаков <ilya.ermakov@sberins.ru>
2021-04-25 11:49:45 +0300

    Init

---
+++ src/main/java/ru/ermakovis/telegram/assistant/Bot.java
@@ -0,0 +1,43 @@
+package ru.ermakovis.telegram.assistant;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.transaction.Transactional;
+
+@Getter
+@Setter
+@Component
+public class Bot extends TelegramLongPollingBot {
+    @Value("${bot.username")
+    private String botUsername = "${bot.name}";
+    @Value("${bot.token}")
+    private String botToken;
+
+    @Transactional
+    @Override
+    public void onUpdateReceived(Update update) {
+        String chatId = update.getMessage().getChatId().toString();
+        String message = update.getMessage().getText();
+
+        sendMsg(chatId, message + " :D");
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/ru/ermakovis/telegram/assistant/AssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ermakovis.telegram.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
